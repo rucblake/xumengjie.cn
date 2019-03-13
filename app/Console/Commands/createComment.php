1fd3151bc7cfd4af89feff576aea83952320956d
杨文清 <yangwenqing001@ke.com>
2019-03-13 12:16:21 +0800

    修改间隔时间

--- app/Console/Commands/createComment.php
+++ app/Console/Commands/createComment.php
@@ -82,7 +82,7 @@ class createComment extends Command
                 ];
                 $this->weiboCommentService->createComment($user, $comment);
                 $result['succeedNum'] ++;
-                sleep(floor(200 / count($users)));
+                sleep(floor(240 / count($users)));
             } catch (\Exception $e) {
                 Log::warning($e->getMessage());
                 $result['failedNum'] ++;
