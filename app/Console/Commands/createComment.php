3529ab35efe38576bde094f5b5a1e49a7ca9412f
rucblake <rucywq@163.com>
2019-04-06 19:30:09 +0800

    修改文案

--- app/Console/Commands/createComment.php
+++ app/Console/Commands/createComment.php
@@ -31,7 +31,7 @@ class createComment extends Command
     protected $weiboUserService;
     protected $weiboCommentService;
 
-    const POSTFIX = '@火箭少女101_徐梦洁#徐梦洁雏菊挚爱香氛中国区推广大使##挚爱雏菊#';
+    const POSTFIX = '@火箭少女101_徐梦洁#多芬品牌大使徐梦洁##徐梦洁rainbow0619生日快乐#';
 
     public function __construct(WeiboService $weiboService,
                                 WeiboUserService $weiboUserService,
